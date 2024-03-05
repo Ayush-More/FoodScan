6ec2d0b7ba0b159c5d29fb966866a8eb0e132c21
SUDHIRsaa <140333096+SUDHIRsaa@users.noreply.github.com>
2024-03-05 21:11:24 +0530

    ohk

--- Foodscan/src/App.jsx
+++ Foodscan/src/App.jsx
@@ -2,11 +2,19 @@ import "./assets/style/global.css";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider} from "react-router-dom";
 import LandPageOverview from "./pages/HomePage.jsx";
+import Upload from './pages/upload.jsx';
+
+
 function App() {
   const router= createBrowserRouter([{
     path: "/",
     element: <LandPageOverview/>,
     },
+    {
+    path: "/upload",
+    element: <Upload/>,
+    },
+    
   ])
   return (
     <div className="App">
--- Foodscan/src/pages/HomePage.jsx
+++ Foodscan/src/pages/HomePage.jsx
@@ -35,7 +35,7 @@ function LandPageOverview() {
             </IconButton>
             <div> 
               <IconButton>
-                 <UploadIcon onClick={()=>{nav ("/profile")}} className="icon " />
+                 <UploadIcon onClick={()=>{nav ("/upload")}} className="icon " />
               </IconButton>
             </div>
     
