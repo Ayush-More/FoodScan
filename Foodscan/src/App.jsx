35e022089cc61fdb4069c905fc8cb67beccb8afc
SUDHIRsaa <140333096+SUDHIRsaa@users.noreply.github.com>
2024-03-05 21:17:18 +0530

    ohk

--- Foodscan/src/App.jsx
+++ Foodscan/src/App.jsx
@@ -2,11 +2,21 @@ import "./assets/style/global.css";
 import { createBrowserRouter } from "react-router-dom";
 import { RouterProvider} from "react-router-dom";
 import LandPageOverview from "./pages/HomePage.jsx";
+import Upload from './pages/upload.jsx' 
+import Search from './pages/Search.jsx'
 function App() {
   const router= createBrowserRouter([{
     path: "/",
     element: <LandPageOverview/>,
     },
+    {
+      path: "/upload",
+      element: <Upload/>,
+    },
+    {
+      path:'/search',
+      element:<Search/>
+    }
   ])
   return (
     <div className="App">
